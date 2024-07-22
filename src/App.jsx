a355226db956ab8b8ce42e25d9b9032b63c465df
wishba <wisnubayu34@gmail.com>
2024-07-22 16:25:42 +0700

    timeline and profile

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,10 @@ function App() {
   const [updateId, setUpdateId] = useState()
   const [updateTodoField, setUpdateTodoField] = useState('')
   const [updateFinishedField, setUpdateFinishedField] = useState(false)
+  const [page, setPage] = useState('timeline')
+  const [timelineOutline, setTimelineOutline] = useState('')
+  const [profileOutline, setProfileOutline] = useState('outline')
+
   const updateRef = useRef()
 
   const fetchTodo = async () => {
@@ -56,6 +60,18 @@ function App() {
       netlifyIdentity.close()
     })
   }, [])
+
+  const handleOpenTimeline = () => {
+    setPage('timeline')
+    setTimelineOutline('')
+    setProfileOutline('outline')
+  }
+
+  const handleOpenProfile = () => {
+    setPage('profile')
+    setTimelineOutline('outline')
+    setProfileOutline('')
+  }
 
   const handleCreate = async e => {
     e.preventDefault()
@@ -131,6 +147,57 @@ function App() {
     updateRef.current.close()
   }
 
+  const postForm = <>
+    <form onSubmit={e => handleCreate(e)}>
+      <textarea
+        style={{ height: "50vh" }}
+        value={createTodoField}
+        onChange={e => setCreateTodoField(e.target.value)}
+      />
+      <input type="submit" value="Post" />
+    </form>
+
+    <dialog ref={updateRef}>
+      <article>
+        <header>
+          <button aria-label="Close" rel="prev"
+            onClick={() => handleUpdateModalClose()}
+          ></button>
+        </header>
+
+        <form onSubmit={e => handleUpdate(e)}>
+          <textarea
+            style={{ height: "70vh" }}
+            value={updateTodoField}
+            onChange={e => setUpdateTodoField(e.target.value)}
+          />
+          <input type="submit" value="Update" />
+        </form>
+      </article>
+    </dialog>
+  </>
+
+  const postContent = <table>
+    <tbody>
+      {allTodo?.map(todo => (
+        <tr key={todo.ref['@ref'].id}>
+          <td>
+            <p>{todo.data.todo}</p>
+          </td>
+
+          <td style={{
+            textAlign: 'right',
+            paddingTop: '0',
+            paddingBottom: '1rem'
+          }}>
+            <button onClick={() => handleUpdateInfo(todo)}>Update</button>
+            <button onClick={() => handleDelete(todo)}>Delete</button>
+          </td>
+        </tr>
+      )).slice().reverse()}
+    </tbody>
+  </table>
+
   return (
     <div className='container'>
       <br />
@@ -147,73 +214,42 @@ function App() {
       </nav>
       <br />
 
-      {netlifyIdentity.currentUser() ?
+      <button
+        className={timelineOutline}
+        style={{ width: '50%' }}
+        onClick={handleOpenTimeline}
+      >Timeline</button>
+      <button
+        className={profileOutline}
+        style={{ width: '50%' }}
+        onClick={handleOpenProfile}
+      >Profile</button>
+      <br />
+      <br />
+
+      {page == 'timeline' ?
         (
-          <>
-            <form onSubmit={e => handleCreate(e)}>
-              <textarea
-                style={{ height: "50vh" }}
-                value={createTodoField}
-                onChange={e => setCreateTodoField(e.target.value)}
-              />
-              <input type="submit" value="Post" />
-            </form>
+          <p>timeline</p>
 
-            <dialog ref={updateRef}>
-              <article>
-                <header>
-                  <button aria-label="Close" rel="prev"
-                    onClick={() => handleUpdateModalClose()}
-                  ></button>
-                </header>
-
-                <form onSubmit={e => handleUpdate(e)}>
-                  <textarea
-                    style={{ height: "70vh" }}
-                    value={updateTodoField}
-                    onChange={e => setUpdateTodoField(e.target.value)}
-                  />
-                  <input type="submit" value="Update" />
-                </form>
-              </article>
-            </dialog>
-          </>
-        ) : (
-          <>
-            <br />
-            <br />
-            <h2 style={{ textAlign: 'center' }}>Hello! Please login to see your post</h2>
-          </>
-        )
+        ) : null
       }
 
-      {isLoadingTodo ?
-        (
-          <p>Loading...</p>
-        ) :
-        (
-          <table>
-            <tbody>
-              {allTodo?.map(todo => (
-                <tr key={todo.ref['@ref'].id}>
-                  <td>
-                    <p>{todo.data.todo}</p>
-                  </td>
+      {page == 'profile' ? (
+        <>
+          {netlifyIdentity.currentUser() ?
+            (<>{postForm}</>) :
+            (<h2 style={{ textAlign: 'center' }}>Please login to see your post</h2>)
+          }
 
-                  <td style={{
-                    textAlign: 'right',
-                    paddingTop: '0',
-                    paddingBottom: '1rem'
-                  }}>
-                    <button onClick={() => handleUpdateInfo(todo)}>Update</button>
-                    <button onClick={() => handleDelete(todo)}>Delete</button>
-                  </td>
-                </tr>
-              )).slice().reverse()}
-            </tbody>
-          </table>
-        )
+          {isLoadingTodo ?
+            (<p>Loading...</p>) :
+            (<>{postContent}</>)
+          }
+        </>
+
+      ) : null
       }
+
     </div>
   )
 }
